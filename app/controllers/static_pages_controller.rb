3ef6e313d9aa2bd97ba69da58df8e29df01cacb2
Ubuntu <ubuntu@ip-172-31-42-171.ap-northeast-1.compute.internal>
2020-04-30 23:17:30 +0000

    Finish static pages (chapter3)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
+  # views/static_pages/home, help
   def home
   end
 
   def help
+  end
+  
+  def about
+  end
+  
+  def contact
   end
 end
