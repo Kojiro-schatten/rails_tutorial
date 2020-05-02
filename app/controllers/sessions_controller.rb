2b620ed1ea7c239f1a7060355c7932d56f8b0074
Ubuntu <ubuntu@ip-172-31-42-171.ap-northeast-1.compute.internal>
2020-05-02 23:57:46 +0000

    implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user 
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,7 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    # logged_in?がtrueの場合に限ってlog_outを呼び出すように変更
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,4 +64,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  
+  test "authenticated? should return false for a user with nil digest" do
+    # 記憶トークンが使われる前にエラーが発生するので、記憶トークンの値は何でも構わないの
+    assert_not @user.authenticated?('')
+  end
 end
