552c90bdd2eced40329eeea51bb7ca24dca1fa86
Ubuntu <ubuntu@ip-172-31-42-171.ap-northeast-1.compute.internal>
2020-05-02 10:14:08 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+  @user = User.new(user_params)
+    if @user.save
+      # 次のコードと等価
+      # redirect_to user_url(@user)
+    # これはredirect_to @userというコードから (Railsエンジニアが) user_url(@user)といったコードを実行したいということを、Railsが推察してくれた結果になります。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      
+    else 
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ApplicationRecord
-    # 長さの強制をしてくれる
-  before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGEX },
-#   メールアドレスが小文字で統一されれば、大文字小文字を区別するマッチが問題なく動作できるからuniqueness: true
-  uniqueness: true
-  
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
+# class User < ApplicationRecord
+#     # 長さの強制をしてくれる
+#   before_save { email.downcase! }
+#   validates :name,  presence: true, length: { maximum: 50 }
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+#   validates :email, presence: true, length: { maximum: 255 },
+#   format: { with: VALID_EMAIL_REGEX },
+# #   メールアドレスが小文字で統一されれば、大文字小文字を区別するマッチが問題なく動作できるからuniqueness: true
+#   uniqueness: true
+  
+#   has_secure_password
+#   validates :password, presence: true, length: { minimum: 6 }
+# end
