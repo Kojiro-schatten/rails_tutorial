3cfb4b18e958da889ffb93f7f80bb216d40b7f76
Ubuntu <ubuntu@ip-172-31-42-171.ap-northeast-1.compute.internal>
2020-05-02 21:37:19 +0000

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,23 +7,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-  @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       # 次のコードと等価
       # redirect_to user_url(@user)
-    # これはredirect_to @userというコードから (Railsエンジニアが) user_url(@user)といったコードを実行したいということを、Railsが推察してくれた結果になります。
-      flash[:success] = "Welcome to the Sample App!"
+      # これはredirect_to @userというコードから (Railsエンジニアが) user_url(@user)といったコードを実行したいということを、Railsが推察してくれた結果になります。
       redirect_to @user
-      
-    else 
+    else
       render 'new'
     end
   end
-  
+
   private
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
